abfc1929ca850c2d19de1ed4193e97422942a222
Dmtro Harazdovskiy <harazdovskiy@gmail.com>
2018-12-29 23:35:00 +0200

    Added CRUD library with fs module for data managing

--- config.js
+++ config.js
@@ -1,29 +1,32 @@
 /**
- * 
+ *
  * Configurational variables
- * 
- * 
+ *
+ *
  */
 
- // Container for all the environments
- const environments = {}
+// Container for all the environments
+const environments = {};
 
- environments.dev = {
-     'httpPort': 3000,
-     'httpsPort': 3001,
-     'envName': 'dev',
- }
- 
- environments.production = {
-    'httpPort': 5000,
-    'httpsPort': 5001,
-    'envName': 'production',
- }
- 
- const currentEnv = typeof(process.env.NODE_ENV) == 'string' ?
-    process.env.NODE_ENV.toLowerCase() : 'dev'
- 
- const environmentToExport = typeof(environments[currentEnv] == 'object') ? 
-    environments[currentEnv] : environments.dev
+environments.dev = {
+    httpPort: 3000,
+    httpsPort: 3001,
+    envName: "dev"
+};
+
+environments.production = {
+    httpPort: 5000,
+    httpsPort: 5001,
+    envName: "production"
+};
+
+const currentEnv =
+    typeof process.env.NODE_ENV == "string"
+        ? process.env.NODE_ENV.toLowerCase()
+        : "dev";
+
+const environmentToExport = typeof (environments[currentEnv] == "object")
+    ? environments[currentEnv]
+    : environments.dev;
 
 module.exports = environmentToExport;
