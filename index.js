90381a916242c68f0c04004d0a2cf0d787e6db06
Dmytro Harazdovskiy <harazdovskiy@gmail.com>
2018-12-30 15:34:42 +0200

    Added users service that links CRUD database actions with http requests

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ const http = require("http");
 const https = require("https");
 const url = require("url");
 const StringDecoder = require("string_decoder").StringDecoder;
-const config = require("./config");
 const fs = require("fs");
-const _data = require("./lib/data");
+
+const config = require("./lib/config");
+const handlers = require('./lib/handles');
+const helpers = require('./lib/helpers');
 
 // Instantiate http server
 const httpServer = http.createServer(function(req, res) {
@@ -74,11 +76,11 @@ const unifiedServer = function(req, res) {
 
         // Construct the data object to send to the handler
         const data = {
-            trimmedPath: trimmedPath,
-            queryStringObject: queryStringObject,
-            method: method,
-            headers: headers,
-            payload: buffer
+            trimmedPath,
+            queryStringObject,
+            method,
+            headers,
+            payload: helpers.parseJsonToObject(buffer),
         };
 
         // Route the request to the handler specified in the router
@@ -101,20 +103,8 @@ const unifiedServer = function(req, res) {
     });
 };
 
-// Define all the handlers
-const handlers = {};
-
-// Sample handler
-handlers.ping = function(data, callback) {
-    callback(200);
-};
-
-// Not found handler
-handlers.notFound = function(data, callback) {
-    callback(404);
-};
-
 // Define the request router
 const router = {
-    ping: handlers.ping
+    ping: handlers.ping,
+    users: handlers.users,
 };
