077aad9e67487d8f0d00fbba4050cecedd16decd
Dmytro Harazdovskiy <harazdovskiy@gmail.com>
2019-01-10 11:34:30 +0200

    Added create check form and fix some misspellings

--- public/app.js
+++ public/app.js
@@ -118,15 +118,15 @@ app.logUserOut = function(redirectUser) {
             : false;
 
     // Send the current token to the tokens endpoint to delete it
-    var payload = {
+    var queryStringObject = {
         tokenId
     };
     app.client.request(
         undefined,
         "api/tokens",
         "DELETE",
+        queryStringObject,
         undefined,
-        payload, 
         function(statusCode, responsePayload) {
             // Set the app.config token as false
             app.setSessionToken(false);
@@ -168,14 +168,43 @@ app.bindForms = function() {
                 var elements = this.elements;
                 for (var i = 0; i < elements.length; i++) {
                     if (elements[i].type !== "submit") {
+                        // Determine class of element and set value accordingly
+                        var classOfElement =
+                            typeof elements[i].classList.value == "string" &&
+                            elements[i].classList.value.length > 0
+                                ? elements[i].classList.value
+                                : "";
                         var valueOfElement =
-                            elements[i].type == "checkbox"
+                            elements[i].type == "checkbox" &&
+                            classOfElement.indexOf("multiselect") == -1
                                 ? elements[i].checked
-                                : elements[i].value;
-                        if (elements[i].name == "_method") {
+                                : classOfElement.indexOf("intval") == -1
+                                    ? elements[i].value
+                                    : parseInt(elements[i].value);
+                        var elementIsChecked = elements[i].checked;
+                        // Override the method of the form if the input's name is _method
+                        var nameOfElement = elements[i].name;
+                        if (nameOfElement == "_method") {
                             method = valueOfElement;
                         } else {
-                            payload[elements[i].name] = valueOfElement;
+                            // Create an payload field named "method" if the elements name is actually httpmethod
+                            if (nameOfElement == "httpmethod") {
+                                nameOfElement = "method";
+                            }
+                            // If the element has the class "multiselect" add its value(s) as array elements
+                            if (classOfElement.indexOf("multiselect") > -1) {
+                                if (elementIsChecked) {
+                                    payload[nameOfElement] =
+                                        typeof payload[nameOfElement] ==
+                                            "object" &&
+                                        payload[nameOfElement] instanceof Array
+                                            ? payload[nameOfElement]
+                                            : [];
+                                    payload[nameOfElement].push(valueOfElement);
+                                }
+                            } else {
+                                payload[nameOfElement] = valueOfElement;
+                            }
                         }
                     }
                 }
@@ -284,6 +313,11 @@ app.formResponseProcessor = function(formId, requestPayload, responsePayload) {
         app.logUserOut(false);
         window.location = "/account/deleted";
     }
+
+    // If the user just created a new check successfully, redirect back to the dashboard
+    if (formId == "checksCreate") {
+        window.location = "/checks/all";
+    }
 };
 
 // Get the session token from localstorage and set it in the app.config object
@@ -336,7 +370,7 @@ app.renewToken = function(callback) {
     if (currentToken) {
         // Update the token with a new expiration
         var payload = {
-            id: currentToken.id,
+            tokenId: currentToken.id,
             extend: true
         };
         app.client.request(
@@ -349,7 +383,7 @@ app.renewToken = function(callback) {
                 // Display an error on the form if needed
                 if (statusCode == 200) {
                     // Get the new token details
-                    var queryStringObject = { id: currentToken.id };
+                    var queryStringObject = { tokenId: currentToken.id };
                     app.client.request(
                         undefined,
                         "api/tokens",
