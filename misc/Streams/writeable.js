2a15ea0ee78618bda2e52fc0c5991c04c55f271c
Dmytro Harazdovskiy <harazdovskiy@gmail.com>
2019-01-18 20:30:56 +0200

    Added examples Readable and Writable streams implementation

---
+++ misc/Streams/writeable.js
@@ -0,0 +1,16 @@
+/**
+ *
+ * Example implementation of writtable stream
+ *
+ */
+
+const { Writable } = require("stream");
+
+const outStream = new Writable({
+    write(chunk, encoding, cb) {
+        console.log(chunk.toString());
+        cb();
+    }
+});
+
+process.stdin.pipe(outStream);
